62ed78cce85d5f81bbb4f4f1ab409e4c684de27d
Junho Kim <takis0112@gmail.com>
2018-06-01 18:58:49 +0900

    clean up

--- SAGAN.py
+++ SAGAN.py
@@ -99,7 +99,7 @@ class SAGAN(object):
                 x = deconv(x, channels=ch // 2, kernel=4, stride=2, sn=self.sn, scope='deconv_'+str(i+1))
                 x = batch_norm(x, is_training, scope='batch_'+str(i))
                 x = relu(x)
-                
+
                 ch = ch // 2
 
             # Self Attention
@@ -179,8 +179,6 @@ class SAGAN(object):
 
         elif self.gan_type == 'wgan-gp' or self.gan_type == 'dragan':
             GP = self.ld * tf.reduce_mean(tf.square(grad_norm - 1.))
-
-
 
         return GP
 
